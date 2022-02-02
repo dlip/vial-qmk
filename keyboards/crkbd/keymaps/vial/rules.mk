b73feac2a2813bc64634b7220d8243bd511e3724
Dane Lipscombe <dane@lipscombe.com.au>
2022-02-02 16:34:44 +1100

    Fix led

--- keyboards/crkbd/keymaps/vial/rules.mk
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -3,10 +3,10 @@ VIA_ENABLE          = yes # Enable VIA
 VIAL_ENABLE         = yes # Enable VIAL
 
 RGBLIGHT_ENABLE     = no # 5832 bytes Lights
-OLED_DRIVER_ENABLE  = yes # 5218 bytes LED screen
+OLED_ENABLE         = yes
 OLED_DRIVER         = SSD1306
 QMK_SETTINGS        = no # 2432 bytes + requires mouse keys
-MOUSEKEY_ENABLE     = yes # 1462 bytes Mouse keys
+MOUSEKEY_ENABLE     = no # 1462 bytes Mouse keys
 COMBO_ENABLE        = no # 1030 bytes Combos eg. a + s = esc
 EXTRAKEY_ENABLE     = yes # 406 bytes Media keys
 VIAL_INSECURE       = yes # 374 bytes (yes = off)
